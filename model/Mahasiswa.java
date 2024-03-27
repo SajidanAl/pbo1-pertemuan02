dcfd285b0b3b592cd5816e5b2c3de708b30677ee
MuhammadSajidanAl-Ghifari <muhammadsajidan2004@gmail.com>
2024-03-27 09:27:19 +0800

    Memanggil method dari banyak Class di package lain

---
+++ model/Mahasiswa.java
@@ -0,0 +1,11 @@
+package model;
+
+public class Mahasiswa{
+ public static void tampilanInfo(){
+  System.out.println();
+  System.out.println("========= Data Mahasiswa ==========");
+  System.out.println("NPM   : 2210010034");
+  System.out.println("Nama  : Muhammad Sajidan AL-Ghifari");
+  System.out.println("===================================");
+ }
+}
